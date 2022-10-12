67751cfc277bfbdbaa6e03714896be5673beddf5
Fouink <yyss9103@gmail.com>
2022-10-12 13:02:54 +0900

    @Query 적용법 연습

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -1,17 +1,22 @@
 package study.datajpa.repository;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
 @Transactional
-//@Rollback(value = false)
+@Rollback(value = false)
 class MemberJpaRepositoryTest {
     @Autowired
     MemberJpaRepository memberJpaRepository;
@@ -30,5 +35,59 @@ class MemberJpaRepositoryTest {
         assertEquals(member.getId(), findMember.getId());
         assertEquals(member.getUsername(), findMember.getUsername());
         assertEquals(member, findMember);
+    }
+
+    @Test
+    public void MemberJpaRepositoryTest() {
+        // given
+        Member member = new Member("member1");
+        Member member1 = new Member("member2");
+
+        // when
+        memberJpaRepository.save(member);
+        memberJpaRepository.save(member1);
+
+
+        Member findMember = memberJpaRepository.findById(member.getId()).get();
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+
+        // then
+        assertEquals(member, findMember);
+        assertEquals(member1, findMember1);
+
+        //리스트 조회 검증
+        List<Member> all = memberJpaRepository.findAll();
+        assertEquals(all.size(), 2);
+
+        //카운트 조회
+        long count = memberJpaRepository.count();
+        assertEquals(count, 2);
+
+        //삭제 검증
+        memberJpaRepository.delete(member);
+        memberJpaRepository.delete(member1);
+
+        long count1 = memberJpaRepository.count();
+        assertEquals(count1, 0);
+
+    }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThenTest() {
+
+        // given
+        Member member = new Member("AAA", 10);
+        Member member1 = new Member("AAA", 20);
+        memberJpaRepository.save(member);
+        memberJpaRepository.save(member1);
+        // when
+
+        List<Member> result = memberJpaRepository.findByUsernameAndAgeGreaterThen("AAA", 15);
+
+        // then
+        assertEquals("AAA", result.get(0).getUsername());
+        assertEquals(20, result.get(0).getAge());
+        assertEquals(1, result.size());
+
     }
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,4 +35,90 @@ class MemberRepositoryTest {
         assertEquals(findMember, member);
 
     }
+
+    @Test
+    public void MemberJpaRepositoryTest() {
+        // given
+        Member member = new Member("member1");
+        Member member1 = new Member("member2");
+
+        // when
+        memberRepository.save(member);
+        memberRepository.save(member1);
+
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+
+        // then
+        assertEquals(member, findMember);
+        assertEquals(member1, findMember1);
+
+        //리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertEquals(all.size(), 2);
+
+        //카운트 조회
+        long count = memberRepository.count();
+        assertEquals(count, 2);
+
+        //삭제 검증
+        memberRepository.delete(member);
+        memberRepository.delete(member1);
+
+        long count1 = memberRepository.count();
+        assertEquals(count1, 0);
+
+    }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThenTest() {
+
+        // given
+        Member member = new Member("AAA", 10);
+        Member member1 = new Member("AAA", 20);
+        memberRepository.save(member);
+        memberRepository.save(member1);
+        // when
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+
+        // then
+        assertEquals("AAA", result.get(0).getUsername());
+        assertEquals(20, result.get(0).getAge());
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    public void findUserTest() {
+        // given
+        Member member = new Member("AAA", 10);
+        Member member1 = new Member("AAA", 20);
+        memberRepository.save(member);
+        memberRepository.save(member1);
+        // when
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+
+        // then
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    public void findUsernameListTest() {
+        // given
+        Member member = new Member("AAA", 10);
+        Member member1 = new Member("BBB", 20);
+        memberRepository.save(member);
+        memberRepository.save(member1);
+
+        // when
+        List<String> usernameList = memberRepository.findUsernameList();
+
+        // then
+        for (String s : usernameList) {
+            System.out.println("s = " + s);
+        }
+    }
+
 }
